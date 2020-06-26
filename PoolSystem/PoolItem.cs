777c6bf8b8b56f1d881b0e92dbbd4ed83b269e2f
DESKTOP\luiz-thiago <contato@luizthiago.com.br>
2020-06-26 15:35:42 -0300

    Added the PoolItem.cs script Added the images for Readme

---
+++ PoolSystem/PoolItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PoolItem : PoolItemMonobehaviour
+{
+    [SerializeField] private UnityEvent onDespawn;
+    [SerializeField] private UnityEvent onSpawn;
+
+    public override void OnDespawn ()
+    {
+        onDespawn?.Invoke ();
+    }
+
+    public override void OnSpawn ()
+    {
+        onSpawn?.Invoke ();
+    }
+}
